7d8a3b266526f78d9521793a356ffa7e848b3276
klauslube <klauslube93@gmail.com>
2022-09-23 12:09:42 -0300

    feat: criado arquivo index com objetos para batalhas

--- src/Battle/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PVE = exports.PVP = void 0;
-const Battle_1 = require("./Battle");
-const PVP_1 = require("./PVP");
-exports.PVP = PVP_1.default;
-const PVE_1 = require("./PVE");
-exports.PVE = PVE_1.default;
-exports.default = Battle_1.default;
